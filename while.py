7d87c15062ca5ac7ff56a139b31715f46810355f
Evelyn19-Ulco <76453340+Evelyn19-Ulco@users.noreply.github.com>
2021-02-05 11:47:42 -0500

    Add files via upload

---
+++ while.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 25 15:19:10 2021
+
+@author: DELL
+"""
+
+x=input('Ingrese el número que contaré')
+x=int(x)
+y=1
+while y<=x:
+    print(y)
+    #y+=1
+    y=y+1
